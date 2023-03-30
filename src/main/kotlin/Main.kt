0e3f219aa923a6acbb0903af56fd372453c53999
Gloria-Lado <glorialadoo@gmail.com>
2023-03-30 19:01:14 +0300

    new

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,14 @@ var data =dataList(listOf("Gloria","Sonia","Apur","Agella","Pualine","Juliet","M
     println(data)
     var high =people(listOf(50,40,2,44,40))
     println(high)
+    var one = Vehicless("UBH",350)
+    var two = Vehicless("KBU",150)
+    var three= Vehicless("TYV",250)
+    var four = Vehicless("UXZ",400)
+
+    var vehicles = listOf(one,two,three,four)
+    println(vehicleList(vehicles))
+    sort()
 }
 //given a list of 10 strings, write a function
 // that returns a list of the strings at even
@@ -28,10 +36,27 @@ fun people(height:List<Int>):String{
 //given a list of people objects, each with the
 // attributes, name , age, height and weight.
 // Sort the list in order of descending age.
-data class peopleList(var name:String, var age:Int,var height:Double,var weight:Int)
+data class PeopleList(var name:String, var age:Int,var height:Double,var weight:Int)
 fun sort(){
-    var list = listOf<peopleList>()
-var person1= peopleList("Gloria",20,6.5,68)
-    var person2= peopleList("Glor",24,8.5,58)
+var person1= PeopleList("Gloria",20,6.5,68)
+    var person2= PeopleList("Glor",24,8.5,58)
+    var totalpeople = listOf<PeopleList>(person1,person2)
+    var total =totalpeople.sortedByDescending {totalpeople->totalpeople.age }
+    println(total)
 
+}
+
+//write a function that takes in a list of car objects each
+// with a registretion and mileage attribute and returns the
+// average mileage of all the vehicles in the list
+
+data class Vehicless(var registration:String, var mileage:Int)
+fun vehicleList(vehicle:List<Vehicless>):Int {
+    var avg = 0
+    vehicle.forEach { new ->
+        new.mileage
+        avg += new.mileage
+    }
+    var totalAvg = vehicle.count()
+    return totalAvg
 }
